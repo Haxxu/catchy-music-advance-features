c373bdfb3e934a80e24847e36caca003b9642383
Haxu <nguyenngocminh190901@gmail.com>
2023-03-10 14:06:38 +0700

    add google login

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const jwt_decode = require('jwt-decode');
 
+const { User } = require('../models/User');
 const ApiError = require('../../utils/ApiError');
 const { validateUser } = require('../models/User');
 const userService = require('../services/UserService');
@@ -10,6 +12,77 @@ const sendMail = require('../../config/sendMail');
 const CLIENT_URL = `${process.env.CLIENT_URL}`;
 
 class AuthController {
+    async login(req, res, next) {
+        try {
+            const user = await User.findOne({ email: req.body.email });
+            if (!user) {
+                return res.status(400).send({ message: 'Invalid email or password' });
+            }
+
+            const validPassword = await bcrypt.compare(req.body.password, user.password);
+            if (!validPassword) {
+                return res.status(400).send({ message: 'Invalid email or password' });
+            }
+
+            const token = user.generateAuthToken();
+
+            return res.status(200).send({ data: token, message: 'Signing in please wait...' });
+        } catch (err) {
+            console.log(err);
+            return next(new ApiError());
+        }
+    }
+
+    async googleLogin(req, res, next) {
+        try {
+            const { token: credential_token } = req.body;
+
+            const decoded = jwt_decode(credential_token);
+
+            const { email, email_verified, name, picture } = decoded;
+
+            if (!email_verified) {
+                next(new ApiError(500, 'Email verification failed.'));
+            }
+
+            const password = email + process.env.GOOGLE_LOGIN_SECRET;
+            const hashedPassword = await bcrypt.hash(password, Number(process.env.SALT));
+
+            const user = await userService.findOne({ email });
+
+            let token;
+
+            if (user) {
+                const validPassword = await bcrypt.compare(password, user.password);
+                if (!validPassword) {
+                    next(new ApiError(400, 'Invalid email or password'));
+                }
+
+                token = user.generateAuthToken();
+            } else {
+                const newUser = {
+                    name,
+                    image: picture,
+                    email,
+                    month: 'january',
+                    date: '1',
+                    year: '2001',
+                    gender: 'male',
+                    password: hashedPassword,
+                };
+
+                const new_user = await userService.createUser(newUser);
+
+                token = new_user.generateAuthToken();
+            }
+
+            return res.status(200).send({ data: token, message: 'Signing in please wait...' });
+        } catch (err) {
+            console.log(err);
+            return next(new ApiError());
+        }
+    }
+
     async register(req, res, next) {
         try {
             const { error } = validateUser(req.body);
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,24 +1,12 @@
 const router = require('express').Router();
-const bcrypt = require('bcrypt');
 
-const { User } = require('../app/models/User');
 const authController = require('../app/controllers/AuthController');
 
 // [POST] api/auth/login -> login and get token
-router.post('/login', async (req, res) => {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-        return res.status(400).send({ message: 'Invalid email or password' });
-    }
+router.post('/login', authController.login);
 
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) {
-        return res.status(400).send({ message: 'Invalid email or password' });
-    }
-
-    const token = user.generateAuthToken();
-    res.status(200).send({ data: token, message: 'Signing in please wait...' });
-});
+// [POST] api/auth/google_login -> google login and get token
+router.post('/google_login', authController.googleLogin);
 
 // [POST] api/auth/register => register (send active token to mail or create)
 router.post('/register', authController.register);
